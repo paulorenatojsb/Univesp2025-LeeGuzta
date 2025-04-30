4a13fd0669f265347834105a51a9cbd6d9f97936
PAULO RENATO BRAGA <paulorenato.jsb@gmail.com>
2025-04-29 23:10:52 -0300

    updt

--- FrontEnd/hamburgueria-frontend/src/index.js
+++ FrontEnd/hamburgueria-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// IMPORTANTE: aqui, o arquivo com @tailwind base/components/utilities
+// IMPORTANTE: aqui só index.css
 import './index.css';
 
 import App from './App';
