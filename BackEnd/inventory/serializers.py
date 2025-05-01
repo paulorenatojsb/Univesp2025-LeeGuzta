4c5cce08dff4b6a72ac2a88d278009bfd8be9769
PAULO RENATO BRAGA <paulorenato.jsb@gmail.com>
2025-05-01 02:24:13 -0300

    dbeaver

--- BackEnd/inventory/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import EstoqueItem
-
-class EstoqueItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EstoqueItem
-        fields = '__all__'
